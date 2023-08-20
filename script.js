55b0ae9efed6f8ffc8d7d0a4ce472a083967c252
Glitch (glitch-hello-website) <none>
2023-08-20 15:33:20 +0000

    🛎📼 Checkpoint ./style.css:57384704/656 ./script.js:57384704/365 ./index.html:57384704/14

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 
 
+// Ejemplo de manejo de eventos para agregar productos al carrito
+const addToCartButtons = document.querySelectorAll('.add-to-cart');
+
+addToCartButtons.forEach(button => {
+    button.addEventListener('click', addToCart);
+});
+
+function addToCart() {
+    // Aquí puedes agregar la lógica para agregar productos al carrito
+    alert('Producto agregado al carrito');
+}
