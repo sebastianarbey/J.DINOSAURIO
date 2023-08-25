b1286d3f875a781243b16ce5a81e47a02850f888
Glitch (glitch-hello-website) <none>
2023-08-25 14:06:32 +0000

    💘🚂 Checkpoint ./script.js:57384704/150

--- script.js
+++ script.js
@@ -2,7 +2,11 @@
 var time = new Date();
 var deltaTime = 0;
 
-if (document.readyState === "complete" )
+if (document.readyState === "complete" || document.readyState === "interactive"){
+setTimeout(Init,1);
+} else{
+document.addEventListener("DOMContentLoaded", Init);
+}
 
 
 
