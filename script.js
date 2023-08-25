294817629a5d896052ce0956c5a5240bf91460c1
Glitch (glitch-hello-website) <none>
2023-08-25 14:30:31 +0000

    🍅🌯 Checkpoint ./script.js:57384704/830

--- script.js
+++ script.js
@@ -15,7 +15,44 @@ Loop();
 }
 
 function loop(){
-deltaTime = (new Date() -time) /
+deltaTime = (new Date() -time) / 1000;
+time = new Date();
+Update()
+requestAnimationFrame(loop);
+}
+
+let sueloY = 22;
+let velY = 0;
+let impulso = 900;
+let gravedad = 2500;
+
+let dinoPosX = 42;
+let dinoPosY = sueloY;
+
+let sueloX = 0;
+let velEscenario = 1280/3;
+let gameVel = 1;
+let score = 0;
+
+let parado = false;
+let saltando = false;
+
+let contenedor;
+let dino;
+let textoScore;
+let suelo;
+let gameOver;
+
+function Start(){
+gameOver = document.querySelector(".game-over");
+suelo = document.querySelector(".suelo");
+contenedor = document.querySelector(".contenedor");
+textoScore = document.querySelector(".score");
+dino = document.querySelector(".game-over");
+}
+
+function Update(){
+
 }
 
 
